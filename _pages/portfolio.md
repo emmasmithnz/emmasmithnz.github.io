2a1a147a0006fd129d6de3ac55980a1b19519277
emmasmithnz <smith.emma.k@gmail.com>
2022-06-25 11:24:34 +1200

    comit

--- _pages/portfolio.md
+++ _pages/portfolio.md
@@ -1,19 +1,8 @@
 ---
-title: Equines
+title: Portfolio
 layout: collection
 permalink: /portfolio/
 collection: portfolio
 entries_layout: grid
 classes: wide
-excerpt: "These are my horses - ka pow"
-gallery:
-  - url: /assets/images/BrightonRock.jpg
-    image_path: assets/images/BrightonRock.jpg
-    alt: "placeholder image 1"
-  - url: /assets/images/BrightonRock.jpg
-    image_path: assets/images/BrightonRock.jpg
-    alt: "placeholder image 2"
-  - url: /assets/images/BrightonRock.jpg
-    image_path: assets/images/BrightonRock.jpg
-    alt: "placeholder image 3"
 ---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,8 +1,14 @@
 var store = [{
-        "title": "Post: Modified Date",
-        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Dad"],
+        "title": "The horses",
+        "excerpt":"This is some horse stuff                                                                                                                                This is a sample gallery to go along with this case study.       and some more stuff   ","categories": [],
+        "tags": [],
+        "url": "/portfolio/something/",
+        "teaser": "/assets/images/BrightonRock.jpg"
+      },{
+        "title": "Post: Horses",
+        "excerpt":"The horses are horses  ","categories": ["Horse"],
         "tags": ["Post Formats","readability","standard"],
-        "url": "/dad/post-modified/",
+        "url": "/horse/post-modified/",
         "teaser": null
       },{
         "title": "Post: Standard",
