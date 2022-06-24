76535745c7ac8521b8c28d0eefe2e5639c9c5630
emmasmithnz <smith.emma.k@gmail.com>
2022-06-25 11:54:15 +1200

    change

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -74,6 +74,12 @@ var store = [{
         "title": "The horses",
         "excerpt":"This is some horse stuff                                                                                                                                This is a sample gallery to go along with this case study.       and some more stuff   ","categories": [],
         "tags": [],
+        "url": "/services/something%20copy%207/",
+        "teaser": "/assets/images/BrightonRock.jpg"
+      },{
+        "title": "The horses",
+        "excerpt":"This is some horse stuff                                                                                                                                This is a sample gallery to go along with this case study.       and some more stuff   ","categories": [],
+        "tags": [],
         "url": "/services/something%20copy/",
         "teaser": "/assets/images/BrightonRock.jpg"
       },{
